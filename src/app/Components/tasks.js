548514c4fd377e3b2cb4acb85d785f641ec8ea0c
J-Daniel-S <59487880+J-Daniel-S@users.noreply.github.com>
2024-05-26 04:21:07 -0500

    finally got react working, websocket open, and began laying out gui. Very rudimentary

---
+++ src/app/Components/tasks.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Tasks = (props) => {
+    return(
+        <main className="collection">
+            <a href="#!" className="collection-item"><span className="new badge">1</span>Alan</a>
+        </main>
+    );
+};
+
+export default Tasks;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.css";
+import Tasks from "./Components/tasks";
+import '../../node_modules/materialize-css/dist/css/materialize.css';
+
+const Home = () => {
+  const WS_URL = "wss://ws.postman-echo.com/raw";
+  const [messageState, setMessageState] = useState([]);
+  const [textState, setTextState] = useState('');
+  const [initialState, setInitialState] = useState(true);
+  // const { sendMessage, lastMessage, readyState} = useWebSocket(WS_URL, {
+  //   share: false,
+  //   shouldReconnect: () => true,
+  // });
+
+  const socket = new WebSocket(WS_URL);
+
+  // Connection opened
+  socket.addEventListener("open", () => {
+    // console.log("connection established");
+    if (initialState) {
+      socket.send("Connection established");
+      setInitialState(false);
+    }
+    
+  });
+
+  socket.addEventListener("message", (event) => {
+    setMessageState(event.data);
+    console.log("Message from server ", event.data);
+  });
+
+  socket.addEventListener('error', (event) => {
+    console.error('WebSocket error:', event);
+  });
+
+  // useEffect(() => {
+
+  // }, [messageState]);
+
+  useEffect(() => {
+    console.log(`new message: ${messageState}`);
+  }, [messageState]);
+
+  const handleChange = e => {
+    if (socket.readyState === WebSocket.OPEN) {
+      setTextState(e.target.value);
+      socket.send(textState);
+    } else {
+      setTimeout(handleChange(), 3);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section>
+        {messageState}
+        <br></br>
+        <section>
+        <div className="row">
+          <form className="col s12">
+            <div className="row">
+              <div className="input-field col s12">
+                <textarea
+                  id="textarea1"
+                  className="materialize-textarea"
+                  value={textState}
+                  onChange={handleChange}
+                ></textarea>
+                <label className="active" for="textarea1">Message to send:</label>
+              </div>
+            </div>
+          </form>
+          <Tasks />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </section>
+      </section>
     </main>
   );
-}
+};
+
+export default Home;
